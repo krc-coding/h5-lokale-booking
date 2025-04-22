30f1fcf785417cc28e95aefbf13479c230cba6ac
Kasper K <Kasperklassen@gmail.com>
2025-04-22 15:29:48 +0200

    auth controller and post man

---
+++ RoomBookingSystem/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    function login(Request $request)
+    {
+        $data = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect username or password.'], 401);
+        }
+
+        $user = auth()->user();
+        $this->logout();
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response(['user' => $user, 'token' => $token]);
+    }
+
+    public function logout()
+    {
+        $user = auth()->user();
+        $userTokens = $user->tokens;
+        foreach ($userTokens as $token) {
+            $token->revoke();
+        }
+    }
+}
