4aa50dcc4c935502fa0d4dd72e16df1f516d32aa
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2025-04-23 11:42:30 +0200

    user controller + postman

---
+++ RoomBookingSystem/app/Http/Controllers/UserController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function getAllUsers()
+    {
+        $user = auth()->user();
+
+        if ($user->role !== 'admin') {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function getUser(user $user)
+    {
+        $user = auth()->user(); 
+
+        if ($user->role !== 'admin' && $authUser->id != $user->id) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+
+        return response(['user' => $user]);
+    }
+
+    public function createUser(Request $request)
+    {
+        $authUser = auth()->user();
+
+        if ($authUser->role !== 'admin' && $authUser->role !== 'systemAdmin') {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $validated = $request->validate([
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+            'role' => 'required|string|in:teacher,admin,',
+        ]);
+
+        $user = User::create([
+            'username' => $validated['username'],
+            'password' => Hash::make($validated['password']),
+            'role' => $validated['role'],
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
+    }
+
+    public function editUser(user $user, Request $request)
+    {
+        $authUser = auth()->user();
+
+        if ($authUser->role !== 'admin' && $authUser->role !== 'systemAdmin') {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $validated = $request->validate([
+            'disabled' => 'boolean',
+            'role' => 'string|in:teacher,admin',
+        ]);
+
+        $user = $user->update([
+            'disabled' => $validated['disabled'] ?? $user->disabled,
+            'role' => $validated['role'] ?? $user->role,
+        ]);
+
+        return response()->json(['message' => 'User updated successfully', 'user' => $user], 201);
+    }
+
+    public function changePassword(user $user, Request $request)
+    {
+        $authUser = auth()->user();
+
+        if ($authUser->role !== 'admin' && $authUser->role !== 'systemAdmin' && $authUser->id != $user->id) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $validated = $request->validate([
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = $user->update([
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json(['message' => 'Password changed successfully', 'user' => $user], 201);
+    }
+
+    public function deleteUser(user $user)
+    {
+        $authUser = auth()->user();
+
+        if ($authUser->role !== 'admin' && $authUser->role !== 'systemAdmin' || $authUser->id == $user->id) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user->delete();
+
+        return response()->json([], 204);
+    }
+}
--- RoomBookingSystem/routes/api.php
+++ RoomBookingSystem/routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
+
+Route::prefix('/user')->group(function () {
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/createUser', [UserController::class, 'createUser']);
+        Route::get('/getUser/{user}', [UserController::class, 'getUser']);
+        Route::get('/getAllUsers', [UserController::class, 'getAllUsers']);
+        Route::put('/editUser/{user}', [UserController::class, 'editUser']);
+        Route::put('/changePassword/{user}', [UserController::class, 'changePassword']);
+        Route::delete('/deleteUser/{user}', [UserController::class, 'deleteUser']);
+
+    });
+});
