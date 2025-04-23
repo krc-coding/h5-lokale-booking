5ecb53f34255a746cccb4e085d4b3f160743ebb1
Kasper K <Kasperklassen@gmail.com>
2025-04-23 09:10:44 +0200

    migrations

---
+++ RoomBookingSystem/database/migrations/2025_04_23_062847_create_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->foreignId('users_id')->constrained();
+            $table->foreignId('rooms_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
