10664b07910a3ab73d1eb6055da2c23d24063914
Kasper K <Kasperklassen@gmail.com>
2025-04-23 11:36:28 +0200

    routes and post man

--- RoomBookingSystem/routes/api.php
+++ RoomBookingSystem/routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
+
+Route::prefix('/room')->group(function () {
+    Route::get('', [RoomController::class, 'getAllRooms']);
+    Route::get('/{room}', [RoomController::class, 'getRoom']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/create', [RoomController::class, 'createRoom']);
+        Route::put('/update/{room}', [RoomController::class, 'updateRoom']);
+        Route::delete('/delete/{room}', [RoomController::class, 'deleteRoom']);
+    });
+});
