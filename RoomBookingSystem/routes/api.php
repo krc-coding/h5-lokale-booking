99a88f807be3e0e2981defb16be4472572be28eb
Kasper K <Kasperklassen@gmail.com>
2025-04-24 09:07:07 +0200

    Update api.php

--- RoomBookingSystem/routes/api.php
+++ RoomBookingSystem/routes/api.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
 
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('/user')->group(function () {
+        Route::post('/createUser', [UserController::class, 'createUser']);
+        Route::get('/getUser/{user}', [UserController::class, 'getUser']);
+        Route::get('/getAllUsers', [UserController::class, 'getAllUsers']);
+        Route::put('/editUser/{user}', [UserController::class, 'editUser']);
+        Route::put('/changePassword/{user}', [UserController::class, 'changePassword']);
+        Route::delete('/deleteUser/{user}', [UserController::class, 'deleteUser']);
+    });
+});
 
 Route::prefix('/room')->group(function () {
     Route::get('', [RoomController::class, 'getAllRooms']);
@@ -17,18 +27,5 @@ Route::prefix('/room')->group(function () {
         Route::post('/create', [RoomController::class, 'createRoom']);
         Route::put('/update/{room}', [RoomController::class, 'updateRoom']);
         Route::delete('/delete/{room}', [RoomController::class, 'deleteRoom']);
-    });
-});
-
-Route::middleware('auth:api')->group(function () {
-    Route::prefix('/user')->group(function () {
-        Route::post('/createUser', [UserController::class, 'createUser']);
-        Route::get('/getUser/{user}', [UserController::class, 'getUser']);
-        Route::get('/getAllUsers', [UserController::class, 'getAllUsers']);
-        Route::put('/editUser/{user}', [UserController::class, 'editUser']);
-        Route::put('/changePassword/{user}', [UserController::class, 'changePassword']);
-        Route::delete('/deleteUser/{user}', [UserController::class, 'deleteUser']);
-
-
     });
 });
