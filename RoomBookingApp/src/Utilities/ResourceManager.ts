3a087eac7e2d1b1cef5c6c1099e09b48ccabc74d
krc-coding <kasper@ulvedalen.dk>
2025-04-28 14:42:42 +0200

    Created simple resource manager wrapper or axios

---
+++ RoomBookingApp/src/Utilities/ResourceManager.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosRequestConfig, Method } from "axios";
+
+export class ResourceManager {
+    private headers: object = { "Accept": "application/json" };
+    private baseUrl: string;
+
+    setAuthTokenHeader = (token: string) => {
+        this.headers = { ...this.headers, "Authorization": "Bearer " + token };
+    }
+
+    setBaseUrl = (baseUrl: string) => {
+        this.baseUrl = baseUrl;
+    }
+
+    makeRequest = (path: string, method: Method, body?: BodyInit, extraOptions: AxiosRequestConfig = {}): RequestHandler => {
+        let options: AxiosRequestConfig = {
+            method: method,
+            headers: this.headers,
+            url: this.baseUrl + path,
+        }
+
+        if (body) {
+            options.data = body;
+        }
+
+        options = { ...extraOptions, ...options }
+
+        return new RequestHandler(options);
+    }
+}
+
+class RequestHandler {
+    private requestConfig;
+
+    constructor(requestConfig: AxiosRequestConfig) {
+        this.requestConfig = requestConfig;
+    }
+
+    getResponse = () => {
+        return axios(this.requestConfig);
+    }
+}
