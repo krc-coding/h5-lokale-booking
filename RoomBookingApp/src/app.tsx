77933d5bc9d7227c4e4109c89749611c692b6f71
krc-coding <kasper@ulvedalen.dk>
2025-05-01 11:19:44 +0200

    Changed root node to root page

--- RoomBookingApp/src/app.tsx
+++ RoomBookingApp/src/app.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import BookingPage from './pages/BookingPage';
 import resourceManager from './Utilities/ResourceManager';
+import RootPage from './pages/RootPage';
 
 resourceManager.setBaseUrl("http://localhost:8000");
 
 const root = createRoot(document.body);
-root.render(<BookingPage />);
+root.render(<RootPage />);
