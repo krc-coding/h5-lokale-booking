33caad36e89c852687372d63c2a09fb7d536835d
krc-coding <kasper@ulvedalen.dk>
2025-05-02 10:27:00 +0200

    Created IPC controller to handle auth token

--- RoomBookingApp/src/preload.ts
+++ RoomBookingApp/src/preload.ts
@@ -1,2 +1,23 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('api', {
+    saveAuthToken: (authToken: string) => {
+        ipcRenderer.send("authToken", {
+            command: "save",
+            token: authToken
+        });
+    },
+    getAuthToken: () => {
+        return ipcRenderer.invoke("authToken", {
+            command: "get",
+        });
+    },
+    deleteAuthToken: () => {
+        ipcRenderer.send("authToken", {
+            command: "delete",
+        });
+    }
+});
