5f04b1ba19a95d3249a80f059eec56491c1ea490
krc-coding <kasper@ulvedalen.dk>
2025-05-06 15:07:38 +0200

    Created blank profile page

---
+++ RoomBookingApp/src/pages/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import resourceManager from "../Utilities/ResourceManager";
+import { Box } from "@mui/material";
+
+const ProfilePage = () => {
+    return (
+        <Box sx={{ height: "100%", width: "100%", backgroundColor: "white" }}>
+        </Box>
+    );
+};
+
+export default ProfilePage;
