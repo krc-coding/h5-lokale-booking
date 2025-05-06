ea42a727ffec27ddc39e0394d7e4632544e8ffc5
krc-coding <kasper@ulvedalen.dk>
2025-05-06 18:50:50 +0200

    Created profile page for current user

--- RoomBookingApp/src/pages/ProfilePage.tsx
+++ RoomBookingApp/src/pages/ProfilePage.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import resourceManager from "../Utilities/ResourceManager";
-import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Card } from "@mui/material";
+import { IUser } from "../types/IUser";
+import ChangePasswordDialog from "../Components/ChangePasswordDialog";
 
 const ProfilePage = () => {
+    const [user, setUser] = useState<IUser>();
+
+    useEffect(() => {
+        setUser(JSON.parse(document.body.dataset["user"] ?? "{}"));
+    }, [document.body.dataset["user"]]);
+
     return (
-        <Box sx={{ height: "100%", width: "100%", backgroundColor: "white" }}>
+        <Box sx={{ height: "100%", width: "100%", display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center" }}>
+            <Card variant="outlined" sx={{
+                width: "50%",
+                height: "50%",
+                backgroundColor: "#EDEDED",
+                display: "flex",
+                flexDirection: "column",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                alignContent: "center"
+            }}>
+                <Box sx={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <Box sx={{ marginBottom: "10px", width: "fit-content" }}>Name: {user?.username}</Box>
+                    <Box sx={{ width: "fit-content" }}>Role: {user?.role}</Box>
+                </Box>
+                <Box sx={{ width: "100%", display: "flex", justifyContent: "space-around" }}>
+                    <ChangePasswordDialog user={user} />
+                </Box>
+            </Card>
         </Box>
     );
 };
